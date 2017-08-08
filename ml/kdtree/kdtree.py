b4144f3e536a17acca4328b8b418c3ef5547fa1b
laomie <hnlaomie@hotmail.com>
2017-08-08 15:20:04 +0800

    nearest neighbour search

--- ml/kdtree/kdtree.py
+++ ml/kdtree/kdtree.py
@@ -20,29 +20,19 @@ class Node(object):
         return (self.left is None and self.right is None)
 
 
-    def set_child(self, index, child):
-        """ Sets one of the node's children
-        index 0 refers to the left, 1 to the right child
-        """
-        if index == 0:
-            self.left = child
-        else:
-            self.right = child
-
-
 class KDTree(object):
-    def __init__(self, data=[], k=2):
+    def __init__(self, data_list=[], k=2):
         self.k = k
-        self.root = self.init_tree(data, 0)
+        self.root = self.init_tree(data_list, 0)
 
-    def init_tree(self, data=[], axis=0) -> Node:
-        if data is None or len(data) == 0:
+    def init_tree(self, data_list=[], axis=0) -> Node:
+        if data_list is None or len(data_list) == 0:
             return None
 
-        if len(data) == 1:
-            return Node(data[0], None, None)
+        if len(data_list) == 1:
+            return Node(data_list[0], None, None)
 
-        sort_data = sorted(data, key = lambda x: x[axis])
+        sort_data = sorted(data_list, key = lambda x: x[axis])
         data_length = len(sort_data)
         split_index = int(data_length / 2)
 
@@ -77,10 +67,62 @@ class KDTree(object):
             if left is not None:
                 stack.append(left)
 
-    def search_nn(self, node_data: []) -> Node:
-        return self.root
+
+    def path_nn(self, root: Node, point: []) -> []:
+        """
+        path from root to leaf for given data
+        :param root: tree node
+        :param point: search data
+        :return:
+        """
+        node_list = []
+        current = root
+        depth = 0
+
+        while current is not None :
+            axis = depth % self.k
+            next = current.left if point[axis] < current.data[axis] else current.right
+            depth += 1
+            node_list.append(current)
+            current = next
+
+        return node_list
+
+
+    def search_nn(self, root: Node, point: []) -> Node:
+        nearest = root
+        prev = None
+
+        # path from root to leaf
+        node_list = self.path_nn(root, point)
+        while len(node_list) > 0:
+            current = node_list.pop()
+
+            if (self.distance(current.data, point) <= self.distance(nearest.data, point)):
+                nearest = current
+                # the other child
+                other_child = current.right if current.left == prev else current.left
+                if other_child is not None:
+                    child_nearest = self.search_nn(other_child, point)
+                    if (self.distance(child_nearest.data, point) < self.distance(nearest.data, point)):
+                        nearest = child_nearest
+
+            prev = current
+
+        return nearest
+
+
+    def distance(self, point1: [], point2: []):
+        length = len(point1)
+        dist = 0
+        for i in range(0, length):
+            dist += (point1[i] - point2[i]) ** 2
+        dist = dist ** 0.5
+        return dist
+
 
 if __name__ == '__main__':
     data = [[1,1], [2,2], [3,3], [4,4], [5,5], [6,6], [7,7]]
     tree = KDTree(data, 2)
-    tree.preorder()
+    nearest = tree.search_nn(tree.root, [5, 5.5])
+    print(nearest.data)
